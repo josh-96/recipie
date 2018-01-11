8dbc631bf2d00ba1120f027e248c6b11f89901f1
Josue <josuevelascomier@gmail.com>
2018-01-10 20:51:58 -0600

    Título agregado

--- recipies/recipie.js
+++ recipies/recipie.js
@@ -1,3 +1,5 @@
+Salmón Picosito con Crema de Aguacate
+
 Mezcla en un bowl el salmón, jugo de limón, aceite de oliva, cilantro, Salsa Huichol Habanero®, la pimienta, la sal, perfectamente y deja reposar por 5 minutos en el refrigerador.
 Muele en la licuadora el aguacate, la crema para batir, la mayonesa, la sal, hasta tener una mezcla homogénea.
 Sirve en vasos salmón, seguido de la crema de aguacate. Decora con los cherry y las ramitas de cilantro.
