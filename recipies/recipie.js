cfa284c725a687a140d6c336cbd99bcfaa5ba8fa
Josue <josuevelascomier@gmail.com>
2018-01-10 20:49:21 -0600

    Preparación de receta

---
+++ recipies/recipie.js
@@ -0,0 +1,3 @@
+Mezcla en un bowl el salmón, jugo de limón, aceite de oliva, cilantro, Salsa Huichol Habanero®, la pimienta, la sal, perfectamente y deja reposar por 5 minutos en el refrigerador.
+Muele en la licuadora el aguacate, la crema para batir, la mayonesa, la sal, hasta tener una mezcla homogénea.
+Sirve en vasos salmón, seguido de la crema de aguacate. Decora con los cherry y las ramitas de cilantro.
